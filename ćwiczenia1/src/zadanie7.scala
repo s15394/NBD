a46e9ccbdcff96991c3871fe8cc4f64818a3b55d
Karol Michalak <s15394@pjwstk.edu.pl>
2020-10-21 15:10:08 +0200

    Ćw 1 ver 1.1 poprawiono literówke w 7

--- ćwiczenia1/src/zadanie7.scala
+++ ćwiczenia1/src/zadanie7.scala
@@ -2,7 +2,7 @@ object zadanie7 {
 //  Zaprezentuj działanie Option na dowolnym przykładzie (np. mapy, w której wyszukujemy wartości po kluczu)
   def main(args: Array[String]): Unit = {
     val capitals: Map[String, String] = Map("Mazowsze" -> "Warszawa", "Pomorskie" -> "Gdańsk")
-    println(s"Stolicą Mazowsza jest ${capitals.get("Warszawa")}")
+    println(s"Stolicą Mazowsza jest ${capitals.get("Mazowsze")}")
     println(s"Stolicą Małopolski jest ${capitals.get("Małopolska")} czego nie ma w kolekcji")
     println(s"Stolicą Małopolski jest ${capitals.getOrElse("Małopolski", "Kraków")} czego nie ma w kolekcji, ale nic nie szkodzi bo mamy getOrElse ")
   }
